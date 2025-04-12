3a6b1302ba8aa584c46eccb1f8b26365a451f756
cookieshake <cookieshake.dev@gmail.com>
2025-04-13 00:21:08 +0900

    add context as arg

--- hatching/workflows/nriy_v1.py
+++ hatching/workflows/nriy_v1.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 from pydantic import BaseModel
+from hatchet_sdk import Context
 
 from hatching import hatchet
 
@@ -12,7 +13,7 @@ class NriyV1Input(BaseModel):
 wf = hatchet.workflow(name="nriy_v1", input_validator=NriyV1Input)
 
 @wf.task()
-def get_now_context(input: NriyV1Input):
+def get_now_context(input: NriyV1Input, ctx: Context):
     return {
         # datetime in KST timezone
         "current_time": datetime.now(timezone(timedelta(hours=9))).strftime("%Y/%m/%d %H:%M"),
