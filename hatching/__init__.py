bfc5fc23283496b02445ef7a8b0e186093e8e0ae
cookieshake <cookieshake.dev@gmail.com>
2025-04-13 01:29:45 +0900

    add propagate handler

--- hatching/__init__.py
+++ hatching/__init__.py
@@ -1,11 +1,23 @@
+import logging
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from hatchet_sdk import Hatchet, ClientConfig
 from loguru import logger
 
+root_logger = logging.getLogger()
+class PropagateHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        root_logger.getChild(record.name).handle(record)
+logger.add(
+    PropagateHandler(),
+    level=logging.DEBUG,
+    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+)
+
 hatchet = Hatchet(
     config=ClientConfig(
-        logger=logger,
+        logger=root_logger,
     )   
 )
