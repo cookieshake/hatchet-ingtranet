250f5894910a6811d3bb98381bab7ce6821ef47a
cookieshake <cookieshake.dev@gmail.com>
2025-04-14 21:54:55 +0900

    change message format

--- hatching/__init__.py
+++ hatching/__init__.py
@@ -6,16 +6,17 @@ load_dotenv()
 from hatchet_sdk import Hatchet, ClientConfig
 from loguru import logger
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 root_logger = logging.getLogger()
 
 class PropagateHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
+        record.message = f"{record.name} | {record.getMessage()}"
         logging.getLogger(record.name).handle(record)
 
 logger.add(
     PropagateHandler(),
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="{message}",
 )
 
